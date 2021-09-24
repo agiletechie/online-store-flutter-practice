451e53b66ef47b5dc36a9b763d20bbf50c0120fc
Shubham Gupta <shubhamgupta7117@gmail.com>
2021-09-24 21:56:09 +0530

    widgets

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id, title, imageUrl;
+  const ProductItem({
+    Key? key,
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(8.0),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.pushNamed(
+              context,
+              ProductDetailScreen.routeName,
+              arguments: id,
+            );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+          title: Text(title),
+          backgroundColor: Colors.black.withOpacity(0.7),
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.favorite,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.shopping_cart),
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+        ),
+      ),
+    );
+  }
+}

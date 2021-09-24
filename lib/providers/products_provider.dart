fcf33b24f7c15787d53fe0ff8c59d03ca5698896
Shubham Gupta <shubhamgupta7117@gmail.com>
2021-09-25 01:09:47 +0530

    separated provider

---
+++ lib/providers/products_provider.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'product.dart';
+
+class ProductsProvider with ChangeNotifier {
+  final List<Product> _products = [
+    Product(
+        id: 'p1',
+        title: 'A Black Shirt',
+        description: 'Branded Shirt',
+        price: 300.0,
+        imageUrl: 'https://images.unsplash.com/'
+            'photo-1583743814966-8936f5b7be1a?'
+            'ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8'
+            'c2hpcnR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto='
+            'format&fit=crop&w=600&q=60'),
+    Product(
+        id: 'p2',
+        title: 'Tissot Watch',
+        description: 'A Branded Tissot Watch',
+        price: 800.0,
+        imageUrl: 'https://images.unsplash.com/'
+            'photo-1522312346375-d1a52e2b99b3'
+            '?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx'
+            '8d2F0Y2h8ZW58MHx8MHx8&ixlib=rb-1.2.1&'
+            'auto=format&fit=crop&w=600&q=60'),
+    Product(
+        id: 'p3',
+        title: 'Alicia Jeans',
+        description: 'Cool Blue Jeans',
+        price: 500.0,
+        imageUrl: 'https://images.unsplash.com/'
+            'photo-1541099649105-f69ad21f3246?'
+            'ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfH'
+            'x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto='
+            'format&fit=crop&w=687&q=80'),
+    Product(
+        id: 'p4',
+        title: 'StreetBond Shoes',
+        description: 'Red Street Bond Shoes',
+        price: 400.0,
+        imageUrl: 'https://images.unsplash.com/'
+            'photo-1560769629-975ec94e6a86?ixlib='
+            'rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by'
+            '1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'),
+  ];
+
+  List<Product> get products {
+    return [..._products];
+  }
+
+  Product findById(String id) {
+    return _products.firstWhere((prod) => prod.id == id);
+  }
+
+  void addProduct() {
+    // _products.add(value);
+    notifyListeners();
+  }
+}

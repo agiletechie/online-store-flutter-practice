b37fd754733356ea2ba24aaad16abc44fde14d78
Shubham Gupta <shubhamgupta7117@gmail.com>
2021-09-26 01:41:01 +0530

    modified data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,74 +1,71 @@
+import 'package:eshop/providers/cart.dart';
 import 'package:flutter/material.dart';
-import '../models/product.dart';
-import '../widgets/product_item.dart';
+import 'package:provider/provider.dart';
+import 'cart_screen.dart';
+import '../widgets/products_gridview.dart';
+import '../widgets/badge.dart';
 
-class HomeScreen extends StatelessWidget {
+enum Filter {
+  favourites,
+  all,
+}
+
+class HomeScreen extends StatefulWidget {
   static const String routeName = 'home-screen';
 
-  HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
 
-  final List<Product> loadedProduct = [
-    Product(
-        id: 'p1',
-        title: 'A Black Shirt',
-        description: 'Branded Shirt',
-        price: 300.0,
-        imageUrl: 'https://images.unsplash.com/'
-            'photo-1583743814966-8936f5b7be1a?'
-            'ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8'
-            'c2hpcnR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto='
-            'format&fit=crop&w=600&q=60'),
-    Product(
-        id: 'p2',
-        title: 'Tissot Watch',
-        description: 'A Branded Tissot Watch',
-        price: 800.0,
-        imageUrl: 'https://images.unsplash.com/'
-            'photo-1522312346375-d1a52e2b99b3'
-            '?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx'
-            '8d2F0Y2h8ZW58MHx8MHx8&ixlib=rb-1.2.1&'
-            'auto=format&fit=crop&w=600&q=60'),
-    Product(
-        id: 'p3',
-        title: 'Alicia Jeans',
-        description: 'Cool Blue Jeans',
-        price: 500.0,
-        imageUrl: 'https://images.unsplash.com/'
-            'photo-1541099649105-f69ad21f3246?'
-            'ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfH'
-            'x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto='
-            'format&fit=crop&w=687&q=80'),
-    Product(
-        id: 'p4',
-        title: 'StreetBond Shoes',
-        description: 'Red Street Bond Shoes',
-        price: 400.0,
-        imageUrl: 'https://images.unsplash.com/'
-            'photo-1560769629-975ec94e6a86?ixlib='
-            'rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by'
-            '1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'),
-  ];
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _showOnlyFavourites = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('E-Shop'),
+        actions: <Widget>[
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
+              child: IconButton(
+                icon: const Icon(
+                  Icons.shopping_cart,
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(context, CartScreen.routeName);
+                },
+              ),
+              value: cart.cartItemCount.toString(),
+            ),
+          ),
+          PopupMenuButton(
+            onSelected: (selectedItem) {
+              setState(() {
+                if (selectedItem == Filter.favourites) {
+                  _showOnlyFavourites = true;
+                } else {
+                  _showOnlyFavourites = false;
+                }
+              });
+            },
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (context) => [
+              const PopupMenuItem(
+                value: Filter.favourites,
+                child: Text('Favourites'),
+              ),
+              const PopupMenuItem(
+                value: Filter.all,
+                child: Text('All'),
+              ),
+            ],
+          ),
+        ],
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10.0),
-        itemCount: loadedProduct.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 1 / 1.25,
-          crossAxisSpacing: 10.0,
-          mainAxisSpacing: 10.0,
-        ),
-        itemBuilder: (context, index) => ProductItem(
-            id: loadedProduct[index].id,
-            title: loadedProduct[index].title,
-            imageUrl: loadedProduct[index].imageUrl),
-      ),
+      body: ProductsGridView(showFavOnly: _showOnlyFavourites),
     );
   }
 }
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const String routeName = 'product-detail-screen';
@@ -8,10 +11,46 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final String productId =
-        ModalRoute.of(context)?.settings.arguments as String;
+        ModalRoute.of(context)!.settings.arguments as String;
+
+    final loadedProduct = Provider.of<ProductsProvider>(context, listen: false)
+        .findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text(productId),
+        title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        //give bounds to SingleChildScrollView
+        child: Column(
+          // mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            SizedBox(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height / 2,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Text(
+              '\$${loadedProduct.price}',
+              style: const TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

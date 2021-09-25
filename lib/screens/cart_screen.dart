bbbf020d259184260bb0f60bd8b70ad91ebfc460
Shubham Gupta <shubhamgupta7117@gmail.com>
2021-09-26 01:36:55 +0530

    cart screen

---
+++ lib/screens/cart_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/cart.dart';
+import '../widgets/cart_item_tile.dart';
+
+class CartScreen extends StatelessWidget {
+  static const String routeName = 'cart-screen';
+
+  const CartScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Cart'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            margin: const EdgeInsets.all(15.0),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  const Text(
+                    'Total',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  const Spacer(),
+                  Chip(
+                    label: Text(
+                      '\$${cart.totalAmount}',
+                      style: const TextStyle(color: Colors.black),
+                    ),
+                    backgroundColor: Theme.of(context).colorScheme.secondary,
+                  ),
+                  TextButton(
+                    child: const Text('ORDER NOW'),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.cartItemCount,
+              itemBuilder: (context, index) => CartItemTile(
+                id: cart.cartItems.values.toList()[index].id,
+                title: cart.cartItems.values.toList()[index].title,
+                price: cart.cartItems.values.toList()[index].price,
+                quantity: cart.cartItems.values.toList()[index].quantity,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
